da01643540c356e58aff2eff4203b5e9438e7bf1
Burak Akkanat <burakakkanat@windowslive.com>
2018-05-24 20:22:21 +0300

    <Here we go>

---
+++ android/app/src/main/java/com/gta_online_business_tracker/MainActivity.java
@@ -0,0 +1,15 @@
+package com.gta_online_business_tracker;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "GTA_Online_Business_Tracker";
+    }
+}
